6cb560001504fae2eb4415f1d74b0b74513cb8c9
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-28 11:28:30 -0500

    TestStack-pushTest

--- gettestie/src/test/java/TestStack.java
+++ gettestie/src/test/java/TestStack.java
@@ -1,2 +1,23 @@
+import gettestie.src.main.java.rocks.zipcode.Person;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Stack;
+
 public class TestStack {
+
+    @Test
+    public void pushTest() {
+        // Given
+        Stack<Person> stack = new Stack<Person>();
+        Person person = new Person("Jerry", 1965);
+        Integer expected = 1;
+
+        // When
+        stack.push(person);
+        Integer actual = stack.size();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
 }
