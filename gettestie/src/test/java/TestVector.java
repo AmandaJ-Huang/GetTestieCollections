01c7237d4910ca249b32e41041626e3e58b8a470
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-30 23:04:52 -0500

    TestVector

--- gettestie/src/test/java/TestVector.java
+++ gettestie/src/test/java/TestVector.java
@@ -1,2 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Vector;
+
 public class TestVector {
+
+    @Test
+    public void testCapacity() {
+        // Given
+        Vector<String> vector = new Vector<>();
+        Integer expected = 10;
+
+        // When
+        Integer actual = vector.capacity(); //default size is 10
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+
 }
