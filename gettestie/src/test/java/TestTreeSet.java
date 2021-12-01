e38e2aeeb305271609cfe5a89f1f9bfad0cea6c3
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-30 23:00:22 -0500

    TestTreeSet

--- gettestie/src/test/java/TestTreeSet.java
+++ gettestie/src/test/java/TestTreeSet.java
@@ -1,2 +1,50 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.TreeSet;
+
 public class TestTreeSet {
+
+    @Test
+    public void testFirst() {
+        // Given
+        String[] fruits = {"dragonfruit", "pear", "apricot", "lychee", "kumquat"};
+        TreeSet<String> treeSet = new TreeSet<>(List.of(fruits));
+        String expected = "apricot";
+
+        // When
+        String actual = treeSet.first();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFloor() {
+        // Given
+        String[] fruits = {"dragonfruit", "pear", "apricot", "lychee", "kumquat"};
+        TreeSet<String> treeSet = new TreeSet<>(List.of(fruits));
+        String expected = "kumquat";
+
+        // When
+        String actual = treeSet.floor("lemons");
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testHeadSet() {
+        // Given
+        String[] fruits = {"dragonfruit", "pear", "apricot", "lychee", "kumquat"};
+        TreeSet<String> treeSet = new TreeSet<>(List.of(fruits));
+        String expected = "[apricot, dragonfruit]";
+
+        // When
+        String actual = treeSet.headSet("kumquat").toString();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
 }
