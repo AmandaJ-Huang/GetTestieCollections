4c9ea5b2d7a15647c6a1d4c3682409f6bcf5ba33
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-30 20:57:35 -0500

    TestIterator

--- gettestie/src/test/java/TestIterator.java
+++ gettestie/src/test/java/TestIterator.java
@@ -1,2 +1,64 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
+
 public class TestIterator {
+
+    @Test
+    public void testNext() {
+        // Given
+        List<String> fruits = new ArrayList<>();
+        fruits.add("apples");
+        fruits.add("oranges");
+        fruits.add("grapes");
+
+        // When
+        Iterator<String> iterator = fruits.iterator();
+
+        // Then
+        Assert.assertEquals("apples", iterator.next());
+        Assert.assertEquals("oranges", iterator.next());
+    }
+
+    @Test
+    public void testHasNext() {
+        // Given
+        List<String> fruits = new ArrayList<>();
+        fruits.add("apples");
+        fruits.add("oranges");
+        fruits.add("grapes");
+
+        // When
+        Iterator<String> iterator = fruits.iterator();
+
+        // Then
+        Assert.assertTrue(iterator.hasNext());
+    }
+
+    @Test
+    public void testRemove() {
+        // Given
+        List<String> fruits = new ArrayList<>();
+        fruits.add("apples");
+        fruits.add("oranges");
+        fruits.add("grapes");
+        String expected = "[apples, grapes]";
+
+        // When
+        Iterator<String> iterator = fruits.iterator();
+        String str = "";
+
+        while (iterator.hasNext()) {
+            str = iterator.next();
+            if (str.equals("oranges")) {
+                iterator.remove();
+            }
+        }
+
+        String actual = fruits.toString();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
 }
